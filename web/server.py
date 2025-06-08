01240a1d1ddff7f05335e3ecfd4ff8b369326b73
Oren Blanco <oren3156@gmail.com>
2025-06-08 21:07:20 +0300

    7

--- web/server.py
+++ web/server.py
@@ -113,16 +113,16 @@ def adjust_threshold():
     updated = False
 
     if action == "delta_increase":
-        set_delta_threshold(get_delta_threshold() + 0.5)
+        set_delta_threshold(get_delta_threshold() + 0.1)
         updated = True
     elif action == "delta_decrease":
-        set_delta_threshold(max(0.001, get_delta_threshold() - 0.5))
+        set_delta_threshold(max(0.001, get_delta_threshold() - 0.1))
         updated = True
     elif action == "raise_increase":
-        set_min_normalized_raise(get_min_normalized_raise() + 0.5)
+        set_min_normalized_raise(get_min_normalized_raise() + 0.1)
         updated = True
     elif action == "raise_decrease":
-        set_min_normalized_raise(max(0.001, get_min_normalized_raise() - 0.5))
+        set_min_normalized_raise(max(0.001, get_min_normalized_raise() - 0.1))
         updated = True
 
     return jsonify({
